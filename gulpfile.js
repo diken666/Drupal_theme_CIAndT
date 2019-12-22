5cbe45711668aa92c3c74bc3c1a22ff0ca23ef4e
diken666 <diken666@163.com>
2019-12-22 16:36:22 +0800

    12/22

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,24 @@
-
-
 let gulp = require('gulp');
+let concat = require('gulp-concat');
+let rename = require('gulp-rename');
 let sass = require('gulp-sass');
-let autoprefixer = require('gulp-autoprefixer');//自动修复插件
+let cleanCSS = require('gulp-clean-css');
+let liveReload = require('gulp-livereload');
 
-// gulp.task('default', function(done) {
-//   console.log("hello");// 将你的默认的任务代码放在这
-//   gulp.series('stream', 'styles');
-//   done();
-// });
-// gulp.task('default', ['stream', 'styles']);
-gulp.task('default', ['sass111']);
-
-// gulp.task('stream', function () {
-//   // Endless stream mode
-//   gulp.watch('css/*.css', ['styles'])//监测scss文件的改动
-// });
-// gulp.task('styles', function() {
-//   gulp.src('sass/*.scss')
-//     .pipe(sass())//把scss转为css
-//     .pipe(sass().on('error', sass.logError))//插入sass日志错误函数，更改默认行为，以便统一修改
-//     .pipe(autoprefixer({
-//       browsers: ['last 2 versions']
-//     }))
-//     .pipe(gulp.dest('./css'));//把转好的css文件存入css文件夹
-// });
-
-gulp.task('sass111', function(){
-  return gulp.src('./scss/test.scss')
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./css/'))
+    .pipe(liveReload())
 });
+
+gulp.task('watch', function () {
+  liveReload.listen();
+  // series 或者 parallel
+  gulp.watch('./sass/*.scss', gulp.parallel('sass'));
+});
+
+
 
 
 
