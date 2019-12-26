6e356de6bd8b01685315e547f7d8983bfdc2764b
diken666 <diken666@163.com>
2019-12-26 11:40:29 +0800

    12/26

--- js/main.js
+++ js/main.js
@@ -21,3 +21,19 @@ window.addEventListener("scroll", function(event) {
     header.classList.remove('white-desktop');
   }
 });
+
+
+// swiper 
+let swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
