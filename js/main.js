cbfd3c08a114c95ec713797f1a6d42ac27303c35
diken666 <diken666@163.com>
2020-01-03 11:56:09 +0800

    1/3

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ let banner = document.getElementsByClassName('banner')[0];
 let bannerChild = banner.children;
 banner.style.height = fullScreenHeight + 'px';
 for (let item of bannerChild) {
-  if ( item.tagName !== 'SPAN'  ) {
+  if ( item.tagName !== 'SPAN' && item.tagName !== 'VIDEO') {
+    console.log(item.tagName)
     item.style.height = fullScreenHeight + 'px';
   }
 }
