ba686f0e90ec2bd69860b8e5dc0717226a821660
diken666 <diken666@163.com>
2020-01-03 12:41:27 +0800

    1/3

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+
 let fullScreenHeight = document.documentElement.clientHeight;
 let banner = document.getElementsByClassName('banner')[0];
-let bannerChild = banner.children;
-banner.style.height = fullScreenHeight + 'px';
-for (let item of bannerChild) {
-  if ( item.tagName !== 'SPAN' && item.tagName !== 'VIDEO') {
-    console.log(item.tagName)
-    item.style.height = fullScreenHeight + 'px';
+if ( banner ) {
+  let bannerChild = banner.children;
+  banner.style.height = fullScreenHeight + 'px';
+  for (let item of bannerChild) {
+    if ( item.tagName !== 'SPAN' && item.tagName !== 'VIDEO') {
+      console.log(item.tagName)
+      item.style.height = fullScreenHeight + 'px';
+    }
   }
 }
+
 
 
 // 监听滚动事件
